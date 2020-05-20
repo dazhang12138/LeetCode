eccd05c34727734b285582813398c389aea1fab7
Mr.Da <836160973da@gmail.com>
2020-05-20 16:35:37 +0800

    4-注释

--- src/com/da/leetcode/one/_4MedianOfTwoSortedArrays.java
+++ src/com/da/leetcode/one/_4MedianOfTwoSortedArrays.java
@@ -11,9 +11,9 @@ import java.util.List;
  */
 public class _4MedianOfTwoSortedArrays {
     public static void main(String[] args) {
-//        Print.printObject(findMedianSortedArrays(new int[]{1,3},new int[]{2}));
-//        Print.printObject(findMedianSortedArrays(new int[]{1,2},new int[]{3,4}));
-//        Print.printObject(findMedianSortedArrays(new int[]{1,3},new int[]{2,4}));
+        Print.printObject(findMedianSortedArrays(new int[]{1,3},new int[]{2}));
+        Print.printObject(findMedianSortedArrays(new int[]{1,2},new int[]{3,4}));
+        Print.printObject(findMedianSortedArrays(new int[]{1,3},new int[]{2,4}));
         Print.printObject(findMedianSortedArrays(new int[]{2},new int[]{}));
     }
 
@@ -45,17 +45,23 @@ public class _4MedianOfTwoSortedArrays {
      * @return 中位数
      */
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //i、j记录两个数组下标；len记录两个数组长度之和；a、b记录中间数（len奇数时a=b）
         int i = -1,j = -1,len = nums1.length + nums2.length, a = 0, b = 0;
+        //lenk为循环次数。len奇数时循环(len+1)/2次，最后一次为中间数；len偶数时循环(len/2+1)次，倒数两次的和/2为中间数；
         int lenk = ((len%2 == 0) ? (len/2+1) : ((len+1)/2));
         for (int k = 0; k < lenk;k++){
             if(j == nums2.length-1){
+                //数组2下标最大值时
                 if(len%2 == 0 && k == lenk-1){
+                    //偶数时b = 最后一次循环；a = 倒数第二次循环；
                     b = nums1[i+1];
                 }else{
+                    //奇数时a = b = 最后一次循环；
                     b = a = nums1[i+1];
                 }
                 i++;
             }else if(i == nums1.length-1 || nums1[i+1] > nums2[j+1]){
+                //数组1下标最大值时 或者 当前坐标下数组1大于数组2
                 if(len%2 == 0 && k == lenk-1){
                     b = nums2[j+1];
                 }else{
@@ -63,6 +69,7 @@ public class _4MedianOfTwoSortedArrays {
                 }
                 j++;
             }else{
+                //当前坐标下数组1小于数组2
                 if(len%2 == 0 && k == lenk-1){
                     b = nums1[i+1];
                 }else{
