ff0ca09978803b65aad3f48e66c892cc28de6a12
Mr.Da <836160973da@gmail.com>
2020-05-25 10:19:56 +0800

    7. 整数反转

---
+++ src/com/da/leetcode/one/_7ReverseInteger.java
@@ -0,0 +1,55 @@
+package com.da.leetcode.one;
+
+import com.da.leetcode.utils.Print;
+
+/**
+ * @author Da
+ * @date 2020/5/25 10:01
+ */
+public class _7ReverseInteger {
+    public static void main(String[] args) {
+//        Print.printObject(reverse(123));
+        Print.printObject(reverse(-123));
+//        Print.printObject(reverse(120));
+    }
+
+    /**
+     * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+     *
+     * 示例 1:
+     *
+     * 输入: 123
+     * 输出: 321
+     *  示例 2:
+     *
+     * 输入: -123
+     * 输出: -321
+     * 示例 3:
+     *
+     * 输入: 120
+     * 输出: 21
+     * 注意:
+     *
+     * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/reverse-integer
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     *
+     * @param x 有符号整数
+     * @return 翻转后有符号整数
+     */
+    public static int reverse(int x) {
+        try {
+            boolean flag = (x < 0) ? false : true;
+            x = flag ? x : -x;
+            StringBuilder stringBuilder = new StringBuilder(String.valueOf(x));
+            Integer back = new Integer(stringBuilder.reverse().toString());
+            return flag ? back : -back;
+        }catch (Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+}
