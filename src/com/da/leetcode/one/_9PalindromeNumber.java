a5dfa23ab4607ce33ce059f5f1c532c85418ba5a
Mr.Da <836160973da@gmail.com>
2020-05-28 09:47:39 +0800

    9.简化算法

--- src/com/da/leetcode/one/_9PalindromeNumber.java
+++ src/com/da/leetcode/one/_9PalindromeNumber.java
@@ -41,13 +41,20 @@ public class _9PalindromeNumber {
      * @return 是否为回文数
      */
     public static boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        for (int i = 0; i < s.length()/2;i++){
-            if(s.charAt(i) != s.charAt(s.length()-i-1)){
-                return false;
-            }
+        if(x < 0){
+            return false;
         }
-        return true;
+        int px = 0;
+        int xx = x;
+        while (x > 0){
+            px = px*10+x%10;
+            x = x/10;
+        }
+        if(px == xx){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
