d858fdee23e353d7105411e0b9d91c971f556a60
Mr.Da <836160973da@gmail.com>
2020-06-09 10:30:03 +0800

    21简化算法减少内存消耗

--- src/com/da/leetcode/_03/_21MergeTwoSortedLists.java
+++ src/com/da/leetcode/_03/_21MergeTwoSortedLists.java
@@ -41,27 +41,18 @@ public class _21MergeTwoSortedLists {
      * @return 合并后链表
      */
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(0);
-        ListNode end = pre;
-        while (l1 != null || l2 != null){
-            if(l1 == null){
-                end.next = l2;
-                break;
-            }
-            if(l2 == null){
-                end.next = l1;
-                break;
-            }
-            if(l1.val <= l2.val){
-                end.next = l1;
-                end = end.next;
-                l1 = l1.next;
-            }else{
-                end.next = l2;
-                end = end.next;
-                l2 = l2.next;
-            }
+        if(l1 == null){
+            return l2;
         }
-        return pre.next;
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val <= l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
     }
 }
