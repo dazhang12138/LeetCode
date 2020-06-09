8c62cbe580e821a075ee64855bac902e0fc56c7b
Mr.Da <836160973da@gmail.com>
2020-06-09 10:26:17 +0800

    ListNode

--- src/com/da/leetcode/utils/ListNode.java
+++ src/com/da/leetcode/utils/ListNode.java
@@ -7,9 +7,12 @@ package com.da.leetcode.utils;
 public class ListNode {
     public int val;
     public ListNode next;
+
     public ListNode(int x) {
         val = x;
     }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public ListNode() {}
 
     @Override
     public String toString() {
